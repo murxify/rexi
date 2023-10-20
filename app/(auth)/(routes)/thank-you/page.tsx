ee1ebae0cedd5f4166fc376a15381dc5a5918c77
murxify <murtaza2k17@gmail.com>
2023-10-20 03:36:33 +0200

    Add thank-you page content

--- app/(auth)/(routes)/thank-you/page.tsx
+++ app/(auth)/(routes)/thank-you/page.tsx
@@ -4,6 +4,10 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
 import { Database } from '@/lib/database.types';
 
+import { Button } from '@/components/ui/button';
+import { SiGmail } from 'react-icons/si';
+import { BiLogoMicrosoft } from 'react-icons/bi';
+
 const ThankYouPage = async () => {
   const cookieStore = cookies();
   const supabase = createServerComponentClient<Database>({
@@ -15,7 +19,31 @@ const ThankYouPage = async () => {
   } = await supabase.auth.getSession();
 
   if (session) redirect('/dashboard');
-  return <div>ThankYouPage</div>;
+
+  return (
+    <div className='h-full flex flex-col justify-center items-center text-center gap-6 px-5'>
+      <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl'>
+        Please verify your email
+      </h1>
+      <h3 className='text-2xl font-semibold tracking-tight text-muted-foreground'>
+        Once you verify your email address, You&apos;ll get access to REXI.
+      </h3>
+      <div className='gap-x-2 flex items-center'>
+        <Button>
+          <SiGmail className='w-6 h-6 mr-2' />
+          <a href='https://www.gmail.com' target='_blank'>
+            Open Gmail
+          </a>
+        </Button>
+        <Button>
+          <BiLogoMicrosoft className='w-6 h-6 mr-2' />
+          <a href='https://www.outlook.com' target='_blank'>
+            Open Outlook
+          </a>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ThankYouPage;

9b591db98565d9219183b8699f11c70659944db0
murxify <murtaza2k17@gmail.com>
2023-10-22 18:22:48 +0200

    Add recharts line in overview

--- app/(dashboard)/_components/overview-card.tsx
+++ app/(dashboard)/_components/overview-card.tsx
@@ -1,27 +1,73 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { LineChart, Line, ResponsiveContainer, Tooltip, XAxis } from 'recharts';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { LucideIcon } from 'lucide-react';
 
 interface OverviewCardProps {
   title: string;
   amount: number;
   Icon: LucideIcon;
+  data: {
+    expense: number;
+    profit: number;
+    revenue: number;
+    date: string;
+  }[];
+  dataKey: 'revenue' | 'profit' | 'expense';
 }
 
-const OverviewCard = ({ title, amount, Icon }: OverviewCardProps) => {
+const OverviewCard = ({
+  title,
+  amount,
+  Icon,
+  data,
+  dataKey,
+}: OverviewCardProps) => {
   return (
     <Card>
       <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
         <CardTitle className='text-sm font-medium'>{title}</CardTitle>
         <Icon className='w-5 h-5 text-muted-foreground' />
       </CardHeader>
-      <CardContent>
-        <div className='text-2xl font-bold'>{amount} kr</div>
-        {/* <p className='text-xs text-muted-foreground'>
-      +20.1% from last month
-    </p> */}
+      <CardContent className='pb-0'>
+        <p className='text-2xl font-bold'>{amount} kr</p>
+        <ResponsiveContainer height={100}>
+          <LineChart data={data}>
+            {/* This line should show the average */}
+            {/* <Line  stroke='#82ca9d' /> */}
+
+            <Line dataKey={dataKey} stroke='#8884d8' />
+            <Tooltip content={<CustomToolTip />} />
+          </LineChart>
+        </ResponsiveContainer>
       </CardContent>
     </Card>
   );
 };
 
 export default OverviewCard;
+
+const CustomToolTip = ({ payload, label, active }: any) => {
+  if (active) {
+    // console.log(payload);
+    // console.log(label);
+    return (
+      <Card>
+        <CardHeader className='p-2 px-4 space-y-0'>
+          <CardTitle className='text-sm'>{payload[0].payload.date}</CardTitle>
+          <CardDescription>{payload[0].name}:</CardDescription>
+        </CardHeader>
+        <CardContent className='p-2 px-4 pt-0'>
+          <p className='text-base font-semibold'>
+            {payload[0].value.toFixed(2)} kr
+          </p>
+        </CardContent>
+      </Card>
+    );
+  }
+};
--- app/(dashboard)/_components/overview.tsx
+++ app/(dashboard)/_components/overview.tsx
@@ -13,6 +13,7 @@ import {
 
 import OverviewCard from './overview-card';
 import EmptyState from './empty-state';
+import { useEffect } from 'react';
 
 const Overview = () => {
   const supabase = createClientComponentClient<Database>();
@@ -50,37 +51,73 @@ const Overview = () => {
   let totalProfits = 0;
   let totalExpenses = 0;
   let avgRevenue = 0;
+  let avgProfits = 0;
+  let avgExpenses = 0;
   profits.forEach((profit) => {
     totalRevenue += profit.revenue;
     totalProfits += profit.profit;
     totalExpenses += profit.expense;
   });
   avgRevenue = totalRevenue / profits.length;
+  avgProfits = totalProfits / profits.length;
+  avgExpenses = totalExpenses / profits.length;
 
   return (
     <>
-      <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-6 py-2'>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-6 py-2'>
         <OverviewCard
           amount={+totalRevenue.toFixed(2)}
           title='Total Revenue'
           Icon={BadgeDollarSign}
+          data={profits}
+          dataKey='revenue'
         />
         <OverviewCard
           amount={+totalProfits.toFixed(2)}
           title='Total Profits'
           Icon={BadgePlus}
+          data={profits}
+          dataKey='profit'
         />
         <OverviewCard
           amount={+totalExpenses.toFixed(2)}
           title='Total Expenses'
           Icon={BadgeMinus}
+          data={profits}
+          dataKey='expense'
+        />
+      </div>
+      {/* Same as above but with average card */}
+      {/* <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-6 py-2'>
+        <OverviewCard
+          amount={+totalRevenue.toFixed(2)}
+          title='Total Revenue'
+          Icon={BadgeDollarSign}
+          data={profits}
+          dataKey='revenue'
+        />
+        <OverviewCard
+          amount={+totalProfits.toFixed(2)}
+          title='Total Profits'
+          Icon={BadgePlus}
+          data={profits}
+          dataKey='profit'
+        />
+        <OverviewCard
+          amount={+totalExpenses.toFixed(2)}
+          title='Total Expenses'
+          Icon={BadgeMinus}
+          data={profits}
+          dataKey='expense'
         />
         <OverviewCard
           amount={+avgRevenue.toFixed(2)}
           title='Avg. Revenue'
           Icon={AlignCenterHorizontal}
+          data={profits}
+          dataKey='revenue'
         />
-      </div>
+      </div> */}
     </>
   );
 };

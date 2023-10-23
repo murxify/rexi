e1bfc1a41a135e57f115d8dcba1fb1d001e82a02
murxify <murtaza2k17@gmail.com>
2023-10-23 17:45:16 +0200

    Add delete function

--- app/(dashboard)/_components/reports/report-actions.tsx
+++ app/(dashboard)/_components/reports/report-actions.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
 import { Database } from '@/lib/database.types';
 
+import { toast } from '@/components/ui/use-toast';
 import { MoreHorizontal } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
@@ -12,42 +15,112 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 
-const ReportActions = ({
-  data,
-}: {
-  data: Database['public']['Tables']['profits']['Row'];
-}) => {
+type DataType = Database['public']['Tables']['profits']['Row'];
+
+const ReportActions = ({ data }: { data: DataType }) => {
+  const [openDelete, setOpenDelete] = useState(false);
+  const [selected, setSelected] = useState<DataType | null>(null);
+
   const queryClient = useQueryClient();
   const supabase = createClientComponentClient<Database>();
 
-  const handleDelete = async (id: number) => {
-    console.log('DELETING', id);
+  const { mutate: deleteById } = useMutation({
+    mutationFn: async (id: number) => {
+      const { data, error } = await supabase
+        .from('profits')
+        .delete()
+        .eq('id', id)
+        .select();
+
+      // data.length === 0 when trying to delete other users revenue
+      if (error || data.length === 0) throw error;
+      return data;
+    },
+    onError: () =>
+      toast({
+        title: 'Something went wrong.',
+        description: 'There was a problem deleting your revenue.',
+        variant: 'destructive',
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['profits'] });
+      toast({
+        description: 'Revenue deleted successfully!',
+        variant: 'success',
+      });
+    },
+  });
+
+  const handleDeleteDialog = (data: DataType) => {
+    setSelected(data);
+    setOpenDelete(true);
   };
 
-  const handleEdit = async (
-    data: Database['public']['Tables']['profits']['Row']
-  ) => {
+  const handleEditDialog = (data: DataType) => {
     console.log('EDITING', data.id);
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className='h-8 w-8 p-0'>
-          <span className='sr-only'>Open menu</span>
-          <MoreHorizontal className='h-4 w-4' />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => handleEdit(data)}>
-          Edit
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleDelete(data.id)}>
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='h-8 w-8 p-0'>
+            <span className='sr-only'>Open menu</span>
+            <MoreHorizontal className='h-4 w-4' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuItem onClick={() => handleEditDialog(data)}>
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => handleDeleteDialog(data)}>
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {/* Delete Dialog */}
+      <AlertDialog
+        open={openDelete}
+        onOpenChange={(isOpen) => {
+          if (isOpen === true) return;
+          setSelected(null);
+          setOpenDelete(false);
+        }}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              revenue from <span>{selected?.date}</span> and remove your data
+              from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => deleteById(selected?.id!)}
+              className='bg-destructive text-destructive-foreground hover:bg-destructive/90'
+            >
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 

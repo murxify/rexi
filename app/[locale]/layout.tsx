666af337707d11e50094dcffe0cc5deeeaf9ca0b
murxify <murxify@gmail.com>
2023-11-01 20:27:02 +0100

    Fix Vercel error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,11 +1,40 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import QueryClientProvider from '@/components/providers/query-client-provider';
 import { I18nProviderClient } from '@/locales/client';
 
-export default function LocaleLayout({
-  params: { locale },
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'REXI | Profit Calculator for Taxi Drivers',
+  description:
+    'Rexi takes care of calculations so you can focus on what matters.',
+};
+
+export default function RootLayout({
   children,
+  params: { locale },
 }: {
-  params: { locale: string };
   children: React.ReactNode;
+  params: { locale: string };
 }) {
-  return <I18nProviderClient locale={locale}>{children}</I18nProviderClient>;
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={inter.className}>
+        <QueryClientProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <I18nProviderClient locale={locale}>{children}</I18nProviderClient>
+          </ThemeProvider>
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
 }
